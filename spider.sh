ab7f1766d980712c27e5675d535fa42ed24d4fed
Michael Pyne <michael.pyne@gmail.com>
2019-07-22 20:09:03 -0400

    Use a specific directory for the output.

--- spider.sh
+++ spider.sh
@@ -9,13 +9,15 @@ set -e -u
 # Location of Navy instructions website
 INST_URL="https://www.secnav.navy.mil/doni/allinstructions.aspx"
 
+mkdir -p out
+
 wget --output-file=wget.log \
      --quiet                \
      --show-progress        \
      --xattr                \
      --no-config            \
      --tries=5              \
-     --wait=3               \
+     --wait=0.5             \
      --random-wait          \
      --no-iri               \
      --no-directories       \
@@ -23,5 +25,6 @@ wget --output-file=wget.log \
      --no-parent            \
      --accept "*.pdf"       \
      --domains=navy.mil     \
+     --directory-prefix=out \
      "$INST_URL"            \
         || echo "There was a problem downloading Navy instructions. wget exit status: $?"
